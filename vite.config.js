140a22563b873f4e4ff83d2262ab099940e8a559
sharedlopezserrano <slopezserrano@byupathway.edu>
2026-02-11 03:05:28 -0300

    Gh pages changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/gaming-catalog/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "src/pages/game.html"),
+      },
+    },
+  },
 });
